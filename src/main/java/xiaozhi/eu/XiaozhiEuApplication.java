20ea2f80b040a8c994c5307a764a8066e8521bcb
MyZhouGit <18312696009@163.com>
2019-04-15 12:31:18 +0800

    Initial commit

---
+++ src/main/java/xiaozhi/eu/XiaozhiEuApplication.java
@@ -0,0 +1,14 @@
+package xiaozhi.eu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class XiaozhiEuApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(XiaozhiEuApplication.class, args);
+  }
+}
