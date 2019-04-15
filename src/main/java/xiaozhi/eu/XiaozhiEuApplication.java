ed219b6557de9ad894c30dfbc9a41f80c36c7ea5
MyZhouGit <18312696009@163.com>
2019-04-15 13:03:00 +0800

    添加注释

--- src/main/java/xiaozhi/eu/XiaozhiEuApplication.java
+++ src/main/java/xiaozhi/eu/XiaozhiEuApplication.java
@@ -7,7 +7,11 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 @SpringBootApplication
 public class XiaozhiEuApplication {
-
+  /**
+   * spring boot 启动
+   *
+   * @param args
+   */
   public static void main(String[] args) {
     SpringApplication.run(XiaozhiEuApplication.class, args);
   }
