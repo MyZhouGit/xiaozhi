508da179ea69f2c6d55e6ea43315d02acf374985
MyZhouGit <18312696009@163.com>
2019-04-23 15:39:22 +0800

    将安全验证的项目加入到spring-clound中

--- src/main/java/xiaozhi/eureka/server/XiaozhiEuApplication.java
+++ src/main/java/xiaozhi/eureka/server/XiaozhiEuApplication.java
@@ -1,48 +1,21 @@
 package xiaozhi.eureka.server;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableEurekaServer
 @SpringBootApplication
 public class XiaozhiEuApplication extends WebSecurityConfigurerAdapter {
-  @Autowired
-  public void securityAuto(AuthenticationManagerBuilder builder) throws Exception {
-    BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-    builder.inMemoryAuthentication().withUser("admin").password(passwordEncoder.encode("admin")).roles("USERS");
-  }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        super.configure(http);
+    }
 
-
-  @Bean
-  public BCryptPasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder();
-  }
-
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    http.authorizeRequests()
-            .anyRequest()
-            .authenticated()
-            .and()
-            //开启默认登录页面
-            .formLogin()
-            //默认登录成功跳转页面
-            .defaultSuccessUrl("/")
-            .permitAll()
-            .and()
-            //设置注销
-            .logout()
-            .permitAll();
-  }
-
-  /**
+    /**
    * spring boot 启动
    *
    * @param args
--- src/test/java/xiaozhi/eureka/server/XiaozhiEuApplicationTests.java
+++ src/test/java/xiaozhi/eureka/server/XiaozhiEuApplicationTests.java
@@ -1,4 +1,4 @@
-package xiaozhi.eu;
+package xiaozhi.eureka.server;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
